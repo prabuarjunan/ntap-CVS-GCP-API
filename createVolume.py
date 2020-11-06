925e1326530f788f7bc9b8be07e42765c5b0fb29
Prabu Arjunan <31085459+prabuarjunan@users.noreply.github.com>
2020-11-05 21:15:59 -0800

    Implemented script for below functions CreateVolume list_volumes() getServiceLevel() updateServiceLevel() getVolumeDetails()

--- createVolume.py
+++ createVolume.py
@@ -10,9 +10,9 @@ from google.oauth2 import service_account
 audience = 'https://cloudvolumesgcp-api.netapp.com'
 server = 'https://cloudvolumesgcp-api.netapp.com'
 service_account_file = '/Users/arjunan/Downloads/ncv-beta-demo-eccee8711557.json'
-project_number = 779740114201
+project_number = 123456789  # Enter your project number here
 location = "us-central1"
-volumeIDdetails = "4f30e00b-87ef-28b3-6e74-72e1c1c378d3"
+volumeIDdetails = "Enter your Volume ID here"
 
 # Small utility function to convert bytes to gibibytes
 def convertToGiB(bytes):
@@ -40,15 +40,16 @@ def createVol():
     id_token1 = get_token()
     # Get all volumes from all regions
     # Construct GET request
+
     createvolumeURL = server + "/v2/projects/" + str(project_number) + "/locations/" + location + "/Volumes/"
     payload = {
-        "name": "AutomatedVolume1",
-        "creationToken": "ACV1",
+        "name": "AutomatedVolume2",
+        "creationToken": "ACV2",
         "region": "us-central1",
         "serviceLevel": "basic",
         "quotaInBytes": 1100000000000,
         "kerberosEnabled": "true",
-        "network": "projects/779740114201/global/networks/ncv-vpc",
+        "network": "projects/123456789/global/networks/ncv-vpc", # Replace with your VPC instead of ncv-vpc and the project number instead of 123456789
         "protocolTypes": [
             "NFSv3"
         ]
@@ -59,18 +60,23 @@ def createVol():
         'Authorization': "Bearer " + id_token1.decode('utf-8'),
         'cache-control': "no-cache",
     }
+    # POST request to create the volume
     response = requests.post(createvolumeURL, json.dumps(payload), headers=headers)
-    #
-    time.sleep(30)
+    # Sleep for 20 seconds to wait for the creation of the volume
+    time.sleep(20)
     r_dict = response.json()
-    #
-    print("Response to POST request: " + response.text)
+    # print("Response to POST request: " + response.text)
     # Get volume attributes
-    #volumeName = (r_dict.get('name'))
+    # To get the values from the dictionary, you have read the dictionary one by one.
+    # fetch the response first
     fetchvalue = (r_dict.get('response'))
+    # fetch all the values from the response
     fetchvolumeID = fetchvalue.get('AnyValue')
+    # fetch the volume ID from the values
     volumeID = fetchvolumeID.get('volumeId')
+    # fetch the service level from the values
     serviceLevel = fetchvolumeID.get('serviceLevel')
+    # Print the values
     print("\tvolumeID: " + volumeID + ", serviceLevel: " + serviceLevel)
 
 createVol()
