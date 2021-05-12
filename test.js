3bcd73cc5ce0a6744cbabd44e0936bb73d12c114
Prabu Arjunan <31085459+prabuarjunan@users.noreply.github.com>
2021-05-12 07:19:38 -0700

    delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const spawn = require("child_process").spawn;
-const pythonProcess = spawn('python',["/Users/arjunan/PycharmProjects/ntap-CVS-GCP-API/get_token.py"]);
-
-pythonProcess.stdout.on('token', (get_token) => {
-  console.log('stdout: ${get_token}');
-});
-pythonProcess.stdout.on('data', (data) => {
-  // Do something with the data returned from python script
-});
